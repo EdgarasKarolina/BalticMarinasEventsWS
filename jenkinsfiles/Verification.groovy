0140e3f1b71ad7e5e13e076ace7d4345a0f912e2
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-10-26 13:26:13 +0200

    Added Build stage to Verification pipeline

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -1,4 +1,8 @@
 node {
 	stage 'Checkout'
 		checkout scm
+
+	stage 'Build'
+		bat 'nuget restore BalticMarinasEventsWS.sln'
+		bat "\"${tool 'MSBuild'}\" BalticMarinasEventsWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
 }
