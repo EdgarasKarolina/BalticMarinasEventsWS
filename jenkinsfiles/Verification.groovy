d7324951d4ce5ec606398da5e0de8db3ee0e2e5e
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-10-26 15:17:40 +0200

    Added Archive stage to Verification pipeline

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -5,4 +5,7 @@ node {
 	stage 'Build'
 		bat '"C:/Program Files/Nuget/bin/nuget.exe" restore BalticMarinasEventsWS.sln'
 		bat "\"${MSBuild}\" BalticMarinasEventsWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+	
+	stage 'Archive'
+		archive 'BalticMarinasEventsWS/bin/Release/**'
 }
