ca0c12c5a080e275b4465870a1d9fdd5d2f62107
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-01 12:41:41 +0100

    Fixed Verification pipeline

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -3,7 +3,7 @@ node {
 		checkout scm
 
 	stage 'Build'
-		bat '"C:/Program Files/Nuget/bin/nuget.exe" restore BalticMarinasEventsWS.sln'
+		bat "\"${MSBuild}\" /t:Restore BalticMarinasEventsWS.sln"
 		bat "\"${MSBuild}\" BalticMarinasEventsWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
 	
 	stage 'Archive'
