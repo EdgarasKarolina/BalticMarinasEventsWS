c47a28742c8f28142d15ad5955e61ade5b801e30
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-02 17:05:20 +0100

    Moved api calls to EventController

--- BalticMarinasEventsWS/Controllers/EventController.cs
+++ BalticMarinasEventsWS/Controllers/EventController.cs
@@ -1,18 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BalticMarinasEventsWS.Models;
+﻿using BalticMarinasEventsWS.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BalticMarinasEventsWS.Controllers
 {
-    public class EventController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IEnumerable<Event> GetAll()
         {
             EventsContext context = HttpContext.RequestServices.GetService(typeof(BalticMarinasEventsWS.Models.EventsContext)) as EventsContext;
-            return View(context.GetAllEvents());
+            return context.GetAllEvents();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Event Get(int id)
+        {
+            EventsContext context = HttpContext.RequestServices.GetService(typeof(BalticMarinasEventsWS.Models.EventsContext)) as EventsContext;
+            return context.GetEventById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
