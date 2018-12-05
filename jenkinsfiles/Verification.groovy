7ec1e12bead4c5909b151cbe58549d11d3a56495
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-05 20:50:15 +0100

    Added email notification when Jenkins job fails

--- jenkinsfiles/Verification.groovy
+++ jenkinsfiles/Verification.groovy
@@ -1,11 +1,57 @@
-node {
-	stage 'Checkout'
-		checkout scm
-
-	stage 'Build'
-		bat "\"${MSBuild}\" /t:Restore BalticMarinasEventsWS.sln"
-		bat "\"${MSBuild}\" BalticMarinasEventsWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
-	
-	stage 'Archive'
-		archive 'BalticMarinasEventsWS/bin/Release/**'
-}
+pipeline
+	{
+	agent none
+		stages
+		{
+			stage('Checkout')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							checkout scm
+						}
+					}
+				}
+			}
+			stage('Build')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							bat "\"${MSBuild}\" /t:Restore BalticMarinasEventsWS.sln"
+							bat "\"${MSBuild}\" BalticMarinasEventsWS.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+						}
+					}
+				}
+			}
+			stage('Archive')
+			{
+				steps
+				{
+					script
+					{
+						node 
+						{
+							archive 'BalticMarinasEventsWS/bin/Release/**'
+						}
+					}
+				}
+			}
+	}
+		post
+		{
+			failure
+				{
+					mail to: 'attendancesystemkea@gmail.com',
+					subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+					body: "Something is wrong with ${env.BUILD_URL}"
+					echo "sent"
+				}
+	    }
+	}
