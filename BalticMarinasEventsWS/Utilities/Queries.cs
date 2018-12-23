c4aec2ba923cbd78c19139ddafa4e9b057d1be64
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-23 17:12:01 +0100

    Made MySQL querries more readable

--- BalticMarinasEventsWS/Utilities/Queries.cs
+++ BalticMarinasEventsWS/Utilities/Queries.cs
@@ -2,13 +2,13 @@
 {
     public static class Queries
     {
-        public const string GetAllEvents = "select * from event";
+        public const string GetAllEvents = "SELECT * FROM event";
 
-        public const string GetEventById = "select * from event where EventId = @eventId";
+        public const string GetEventById = "SELECT * FROM event WHERE EventId = @eventId";
 
-        public const string GetAllEventsByUserId = "select * from event where UserId = @userId";
+        public const string GetAllEventsByUserId = "SELECT * FROM event WHERE UserId = @userId";
 
-        public const string DeleteEventById = "delete from event where EventId = @eventId";
+        public const string DeleteEventById = "DELETE FROM event WHERE EventId = @eventId";
 
         public const string CreateEvent = "INSERT INTO event (Title, Location, Period, Description, UserId)\n" +
                     "VALUES (@title, @location, @period, @description, @userId);";
